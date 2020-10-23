cd017d2ba1c44e76b665620b5e9350a5affd1299
Gabriel Augusto <gabriel.rsilva@bandtec.com.br>
2020-10-23 13:23:42 -0300

    Adicionando estrutura da API

---
+++ src/auaupi/core.clj
@@ -0,0 +1,7 @@
+(ns auaupi.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/auaupi/core_test.clj
@@ -0,0 +1,7 @@
+(ns auaupi.core-test
+  (:require [clojure.test :refer :all]
+            [auaupi.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
