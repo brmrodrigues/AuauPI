efba76b4e60c7a3ed3b4091795080cb1680f3c09
Gabriel Augusto <gabriel.rsilva@bandtec.com.br>
2020-10-28 11:59:31 -0300

    Configurando servidor pedestal

--- src/auaupi/core.clj
+++ src/auaupi/core.clj
@@ -17,3 +17,43 @@
          {:id "1" :name "Leka" :breed "Pincher" :url "https://images.dog.ceo/breeds/maltese/n02085936_4781.jpg" :age 8 :gender "F" :castrated? true :port "P" :adopted? false}
          {:id "2" :name "Xenon" :breed "Weimaraner" :url "https://images.dog.ceo/breeds/weimaraner/n02092339_747.jpg" :age 2 :gender "M" :castrated? false :port "G" :adopted? false}
          {:id "3" :name "Thor" :breed "Pitbull" :url "https://images.dog.ceo/breeds/pitbull/IMG_20190826_121528_876.jpg" :age 7 :gender "M" :castrated? true :port "G" :adopted? false}]))
+
+(defn respond-hello [request]
+  {:status 200 :body "Servidor funcionando"})
+
+(def routes
+  (route/expand-routes
+    #{["/" :get respond-hello :route-name :greet]}))
+
+(def pedestal-config
+    (-> {::http/routes routes
+       ::http/type :jetty
+       ::http/join? false
+       ::http/port 3000}
+      http/default-interceptors
+      (update ::http/interceptors conj (body-params/body-params))))
+
+(defn start []
+  (http/start (http/create-server pedestal-config)))
+
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                        (assoc pedestal-config
+                          ::http/join? false)))))
+
+
+(defn stop-dev []
+  (http/stop @server))
+
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
+
+
+#_(restart)
+#_(start-dev)
+#_(stop-dev)
