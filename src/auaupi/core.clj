093badd299e1eb0d28234f86e7cd92db476db84c
MarkzBandtec <vitor.portela@bandtec.com.br>
2020-10-28 11:56:07 -0300

    reoganização do atom dogs

--- src/auaupi/core.clj
+++ src/auaupi/core.clj
@@ -8,10 +8,44 @@
   (:gen-class))
 
 (def dogs
-  (atom [{:id "0" :name "Bardock" :breed "Mix" :url "https://images.dog.ceo/breeds/mix/piper.jpg" :age 15 :gender "M" :castrated? true :port "M" :adopted? false}
-         {:id "1" :name "Leka" :breed "Pincher" :url "https://images.dog.ceo/breeds/maltese/n02085936_4781.jpg" :age 8 :gender "F" :castrated? true :port "P" :adopted? false}
-         {:id "2" :name "Xenon" :breed "Weimaraner" :url "https://images.dog.ceo/breeds/weimaraner/n02092339_747.jpg" :age 2 :gender "M" :castrated? false :port "G" :adopted? false}
-         {:id "3" :name "Thor" :breed "Pitbull" :url "https://images.dog.ceo/breeds/pitbull/IMG_20190826_121528_876.jpg" :age 7 :gender "M" :castrated? true :port "G" :adopted? false}]))
+  (atom [{:id "0" 
+          :name "Bardock" 
+          :breed "Mix" 
+          :url "https://images.dog.ceo/breeds/mix/piper.jpg" 
+          :age 15 :gender "M" 
+          :castrated? true 
+          :port "M" 
+          :adopted? false}
+         
+         {:id "1" 
+          :name "Leka" 
+          :breed "Pincher" 
+          :url "https://images.dog.ceo/breeds/maltese/n02085936_4781.jpg" 
+          :age 8 
+          :gender "F" 
+          :castrated? true 
+          :port "P"
+          :adopted? false}
+         
+         {:id "2" 
+          :name "Xenon" 
+          :breed "Weimaraner"
+          :url "https://images.dog.ceo/breeds/weimaraner/n02092339_747.jpg" 
+          :age 2 
+          :gender "M" 
+          :castrated? false 
+          :port "G" 
+          :adopted? false}
+         
+         {:id "3" 
+          :name "Thor" 
+          :breed "Pitbull" 
+          :url "https://images.dog.ceo/breeds/pitbull/IMG_20190826_121528_876.jpg" 
+          :age 7 
+          :gender "M" 
+          :castrated? true 
+          :port "G" 
+          :adopted? false}]))
 
 (defn return-all [_req]
   (map #(into {}
