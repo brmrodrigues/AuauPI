13f6a29284ff6e2ed41210b4c53239d3c6053d20
MarkzBandtec <vitor.portela@bandtec.com.br>
2021-01-06 14:58:54 -0300

    ns swagger criado

---
+++ src/auaupi/swagger.clj
@@ -0,0 +1,5 @@
+(ns auaupi.swagger
+  (:require [io.pedestal.interceptor.helpers :as interceptor]
+            [schema.core :as schema]
+            [pedestal.swagger.core :as swagger]
+            [pedestal.swagger.doc :as swagger.doc]))
