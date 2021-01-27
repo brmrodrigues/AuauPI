46a57be062cfbbbbfef2aa6c5dc2df71789f882a
MarkzBandtec <vitor.portela@bandtec.com.br>
2021-01-27 09:14:47 -0300

    swagger ainda inconsistente

--- swagger/auaupi/swagger.clj
+++ swagger/auaupi/swagger.clj
@@ -1,14 +1,22 @@
 (ns auaupi.swagger
   (:require [io.pedestal.http :as http]
-            [io.pedestal.test :refer [response-for]]
+            [io.pedestal.test :as p.test]
             [clojure.java.io :as io]
             [auaupi.service :as service]))
 
-(defn -main []
+(defn response-swagger []
   (let [service (::http/service-fn (http/create-server service/pedestal-config))
-        {:keys [status body]} (response-for service :get "/auaupi/swagger.json")]
-    (assert (= 200 status))
-    (io/make-parents "doc/swagger/swagger.json")
-    (spit "doc/swagger/swagger.json" body))
+        response (-> service
+                     (p.test/response-for :get "/auaupi/swagger.json"))]
+      response))
+
+(defn -main []
+  (loop []
+    (if (not (= (:status (response-swagger)) 404))
+      (do
+        (println "deu certo")
+        (io/make-parents "doc/swagger/swagger.json")
+        (spit "doc/swagger/swagger.json" (:body (response-swagger))))
+      (recur)))
   (shutdown-agents)
   (System/exit 0))
