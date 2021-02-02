bc2a15110e60a0cc49e8e0bb564b750ac79feb5e
MarkzBandtec <vitor.portela@bandtec.com.br>
2021-02-02 14:41:12 -0300

    retirando schema antigo()

--- src/auaupi/routes.clj
+++ src/auaupi/routes.clj
@@ -13,17 +13,6 @@
    [schema.core :as s]
    [auaupi.schema :as schema]))
 
-#_(s/defschema Dog
-  {:id s/Int
-   :name s/Str
-   :breed s/Str
-   :image s/Str
-   :birth s/Str
-   :gender (s/enum "m" "f")
-   :port (s/enum "p" "m" "g")
-   :castrated? s/Bool
-   :adopted? s/Bool})
-
 (defn get-dogs [ctx]
   (let [req (get ctx :request)
         result (-> req
